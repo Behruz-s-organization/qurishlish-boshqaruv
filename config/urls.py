172e2d23c099ff08bc87cd4356eb4f38f4b9b614
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-11-19 17:18:16 +0500

    packagelarni ornatib sozlab chiqish kerak #2: issue bajarildi

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,38 @@
+# django
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+# rest framework
+from rest_framework import permissions
+# drf yasg
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Qurilish Boshqaruv API",
+      default_version='v1',
+      description="Qurilish boshqaruv api",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="xoliqberdiyevbehruz12@gmail.com"),
+      license=openapi.License(name="Behruz's-Organization License"),
+   ),
+   public=False,
+   permission_classes=(permissions.IsAdminUser,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+
+urlpatterns += [
+   path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
